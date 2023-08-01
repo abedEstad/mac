2f08db95d7316ffbc7d2b235aca28af5897d23df
abedEstad <72113833+abedEstad@users.noreply.github.com>
2023-08-01 18:21:19 -0500

    Create macArchivo.py

---
+++ macArchivo.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from openpyxl import reader,load_workbook,Workbook
+
+st.title(“FK_KM CHECK”)
+
+fkonzept = st.file_uploader(“upload FK file”, type={“xlsx”,“csv”, “txt”})
+
+wb=Workbook()
+
+if fkonzept is not None:
+wb =load_workbook(fkonzept, read_only=True)
+st.write(wb.sheetnames)
+st.title(wb)
+st.write(wb.active)
+
+wb.save(‘fk.xlsx’)
+
+st.download_button(
+label=“Download Excel worksheet without index”,
+data=buffer,
+file_name=“fk.xlsx”,
+mime=“application/vnd.ms-excel”,)
